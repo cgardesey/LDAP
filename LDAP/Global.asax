7435f8bc4790bb94c436284b92f2fda4254f0de8
Cyril Gardesey <cyrilgardesey@gmail.com>
2023-10-01 20:12:53 +0000

    first commit

---
+++ LDAP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LDAP.MvcApplication" Language="C#" %>
---
+++ LDAP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LDAP.Startup))]
+namespace LDAP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
